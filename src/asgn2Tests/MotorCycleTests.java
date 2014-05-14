35077b2060ab6891553f6ac2c6f98773acac3f20
Thomas McCarthy <tom@tomm.cc>
2014-05-14 19:37:30 +1000

    More MotorCycle Tests & Created MotorCycle class

--- src/asgn2Tests/MotorCycleTests.java
+++ src/asgn2Tests/MotorCycleTests.java
@@ -17,19 +17,25 @@ import org.junit.Before;
 import org.junit.Test;
 
 import asgn2Exceptions.VehicleException;
+import asgn2Vehicles.Car;
 import asgn2Vehicles.MotorCycle;
 import asgn2Vehicles.Vehicle;
 
-/**
- * @author hogan
- *
- */
+
 public class MotorCycleTests {
 	private static final String EXAMPLE_PLATE = "1234Test";
 	private static final int EXAMPLE_TIME = 10;
-
-
 	private Vehicle testBike;
+
+	@Before
+	/**
+	 * Creates a new bike before every test
+	 * @author Thomas McCarthy
+	 */
+	public void testCreateTestBike() throws VehicleException {
+		testBike = new MotorCycle (EXAMPLE_PLATE, EXAMPLE_TIME);
+	}
+
 	
 	/**
 	 * Testing the creation of a normal motorcycle Object
@@ -38,8 +44,19 @@ public class MotorCycleTests {
 	 */
 	@Test
 	public void testNewMotorcycleCreation() throws VehicleException{
-		testBike = new MotorCycle (EXAMPLE_PLATE, EXAMPLE_TIME);
 		assertTrue(testBike != null);
+	}
+	
+	/**
+	 * Tests if the constructor throws an exception if provided arrival
+	 * time <= 0
+	 * @author Thomas McCarthy
+	 * @throws VehicleException
+	 */
+	@Test (expected = VehicleException.class)
+	public void testLowArrivalTime() throws VehicleException {
+		
+		testBike = new MotorCycle(EXAMPLE_PLATE, 0);
 	}
 	
 }
